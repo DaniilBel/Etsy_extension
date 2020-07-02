8ab686c6d91d7fac02de66e57e337e8a8d46c214
Daniil <danya.belyaev@gmail.com>
2020-07-02 18:57:31 +0300

    Update background.js

--- background.js
+++ background.js
@@ -5,7 +5,7 @@ chrome.runtime.onInstalled.addListener(function () {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {hostEquals: 'developer.chrome.com'}
+                pageUrl: {hostEquals: 'learn.javascript.ru'}
             })
             ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
